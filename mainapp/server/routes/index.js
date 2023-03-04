7e82d7d7b876c66643d6ef05f26780e02bb3b418
nyondoo <ahfl1129@gmail.com>
2023-03-04 19:05:55 +0900

    feat: 배포오류수정

--- mainapp/server/routes/index.js
+++ mainapp/server/routes/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controller/main');
-const login_controller = require('../controller/login');
-const passport = require('passport-naver');
 
 router.get('/', controller.main);
 router.post('/workInfo', controller.workInfo);
